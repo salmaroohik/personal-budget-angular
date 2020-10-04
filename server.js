398ac41b3b5f422d48c2695ac71460d4d0831dc5
salmaroohik <skhayum@uncc.edu>
2020-10-04 14:54:55 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -3,24 +3,9 @@ const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
+'use strict';
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 30
-    },
-    {
-        title: 'Rent',
-        budget: 350
-    },
-    {
-        title: "Groceries",
-        budget:90
-    },
-
-]
-};
+const budget = require('./budget.json');
 
 app.get('/hello', (req,res)=>{
     res.send('Hello World!');
