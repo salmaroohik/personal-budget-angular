d1ec1a865aa8e9c2404915b0e32d473ca85f9ebf
salmaroohik <skhayum@uncc.edu>
2020-10-07 16:17:59 -0400

    CORS added

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express =require('express');
 const app = express();
+const cors =require('cors');
 const port = 3000;
 
-app.use('/', express.static('public'));
-'use strict';
+app.use(cors());
 
 const budget = require('./budget.json');
 
-app.get('/hello', (req,res)=>{
-    res.send('Hello World!');
-});
+
 
 app.get('/budget', (req,res)=>{
     res.json(budget);
 });
 
 app.listen(port,() =>{
-    console.log('Example app listening at http://localhost:${port}')
+    console.log('Example API listening at http://localhost:${port}')
 });
